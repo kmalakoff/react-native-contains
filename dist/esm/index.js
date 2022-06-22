dbb1fdfd44f1c0ef042755ae05e8be554ee59524
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-22 09:22:05 -0700

    Delete unused

--- dist/esm/index.js
+++
@@ -1,2 +0,0 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.default=contains;function containsTag(node,tag){if(!node._nativeTag){return node===tag;}if(node._nativeTag===tag){return true;}for(var child of node._children){if(containsTag(child,tag)){return true;}}return false;}function contains(element,target){if(element.contains){return element.contains(target);}return containsTag(element,target._nativeTag);}
-//# sourceMappingURL=index.js.map
