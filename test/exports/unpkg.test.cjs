c9e7f24b5baa7406ae515697605db5f06efd1572
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-22 19:14:26 -0800

    Upgrade modules

--- test/exports/unpkg.test.cjs
+++ test/exports/unpkg.test.cjs
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const assert = require('assert');
 const contains = require('react-native-contains/dist/umd/react-native-contains.min.js');
 
-describe('exports react-native-contains/dist/umd/react-native-contains.js', function () {
-  it('defaults', function () {
+describe('exports react-native-contains/dist/umd/react-native-contains.js', () => {
+  it('defaults', () => {
     assert.equal(typeof contains, 'function');
   });
 });
