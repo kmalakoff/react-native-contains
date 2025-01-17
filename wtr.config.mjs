c62d22741d3b0303c8496a45c120db04ff9fc15c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-17 12:03:30 -0800

    Update web test runner port to 9010

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -2,7 +2,7 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 import createConfig from 'tsds-web-test-runner/createConfig.mjs';
 
 export default createConfig({
-  port: 8009,
+  port: 9010,
   plugins: [
     importMapsPlugin({
       inject: {
