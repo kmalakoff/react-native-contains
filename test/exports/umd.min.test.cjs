a22d5ce8f2447bd97cd3db0f481e8cc3fe2155b4
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 17:48:02 -0700

    Update package exports and type definitions

--- test/exports/umd.min.test.cjs
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const contains = require('react-native-contains/dist/umd/react-native-contains.min.cjs');
-
-describe('exports react-native-contains/dist/umd/react-native-contains.min.cjs', () => {
-  it('defaults', () => {
-    assert.equal(typeof contains, 'function');
-  });
-});
